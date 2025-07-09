c20c2fbb522ef44c4308c3adec9b5a6f62b349f1
eduardamaia <eduardamfmcavalcanti@gmail.com>
2025-07-09 11:40:07 -0300

    ajustes no html

--- projeto_django/confeitaria/forms.py
+++ projeto_django/confeitaria/forms.py
@@ -22,10 +22,13 @@ class PedidoForm(forms.ModelForm):
     class Meta:
         model = Pedido
         fields = ['cliente']  # data_pedido é automático
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.cliente.nome} - {self.data_pedido.strftime('%d/%m/%Y %H:%M')}"
 
 class PedidoProdutoForm(forms.ModelForm):
     class Meta:
         model = PedidoProduto
         fields = ['id_produto', 'quantidade']
-        
-        
+    
+    def __str__(self):
+        return f"{self.produto.nome} (Qtd: {self.quantidade})"
