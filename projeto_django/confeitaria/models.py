a554a0417f862a04813fd2b3f67217e32ff1cb8b
eduardamaia <eduardamfmcavalcanti@gmail.com>
2025-07-11 16:13:27 -0300

    login

--- projeto_django/confeitaria/models.py
+++ projeto_django/confeitaria/models.py
@@ -29,3 +29,10 @@ class PedidoProduto(models.Model):
     id_pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     id_produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField(default=1)
+    
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length = 50)
+    senha = models.CharField(max_length = 50)
+    def __str__(self):
+        return self.usuario
--- projeto_django/confeitaria/views.py
+++ projeto_django/confeitaria/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db import transaction
-from .forms import ProdutoForm, ClienteForm, PedidoForm, PedidoProdutoForm
-from .models import Pedido, Cliente, Produto, PedidoProduto
+from django.contrib import messages
+from .forms import ProdutoForm, ClienteForm, PedidoForm, PedidoProdutoForm, UsuarioForm
+from .models import Pedido, Cliente, Produto, PedidoProduto, Usuario
+from django.contrib import messages
+from django.contrib.auth import login as django_login, get_user_model
+from django.db import connection
+from django.contrib.auth.hashers import check_password
+
 
 def menu(request):
     return render(request, 'confeitaria/menu.html')
@@ -58,3 +64,31 @@ def adicionar_produto_ao_pedido(request, id_pedido):
 def listar_pedidos(request):
     pedidos = Pedido.objects.all().order_by('-data_pedido') # Added ordering for better display
     return render(request, 'confeitaria/lista_pedidos.html', {'pedidos': pedidos})
+
+def autenticar_login(request):
+    if request.method == "POST":
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            user_input  = request.POST.get('usuario')
+            pwd_input   = request.POST.get('senha')
+
+            with connection.cursor() as cur:
+                cur.execute(
+                    "SELECT senha FROM confeitaria_usuario WHERE usuario = %s",
+                    [user_input]
+                )
+                row = cur.fetchone()
+
+            if row and pwd_input == row[0]:
+                User = get_user_model()
+                django_user, _ = User.objects.get_or_create(
+                    username=user_input, defaults={"is_active": True}
+                )
+                django_login(request, django_user)  # cria sessão
+                return redirect("menu")
+
+            messages.error(request, "Usuário ou senha incorretos.")
+    else:
+        form = UsuarioForm()
+
+    return render(request, "confeitaria/login.html", {"form": form})
